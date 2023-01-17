4a932ec4ef1a23dfe1215318aadb9f6d670400b1
ro-el-c <chung3286@naver.com>
2023-01-17 21:03:46 +0900

    3. Spring Core 1 - Apply OOP - AppConfig Refactoring

--- src/main/java/hello/springcore/AppConfig.java
+++ src/main/java/hello/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.springcore;
 
+import hello.springcore.discount.DiscountPolicy;
 import hello.springcore.discount.FixDiscountPolicy;
+import hello.springcore.member.MemberRepository;
 import hello.springcore.member.MemberService;
 import hello.springcore.member.MemberServiceImpl;
 import hello.springcore.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import hello.springcore.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
