eba5921364c0ffbbc02474cf7572d634b9d51f5f
ro-el-c <chung3286@naver.com>
2023-01-17 23:46:11 +0900

    3. Spring Core 1 - Apply OOP - to Spring

--- src/main/java/hello/springcore/MemberApp.java
+++ src/main/java/hello/springcore/MemberApp.java
@@ -4,12 +4,24 @@ import hello.springcore.member.Grade;
 import hello.springcore.member.Member;
 import hello.springcore.member.MemberService;
 import hello.springcore.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        /**
+         * 어노테이션 기반 구성
+         * 파라미터 AppConfig.class -> AppConfig 에 있는 환경 설정 정보를 스프링 컨테이너에 등록하고 관리
+         * */
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        /**
+         * AppConfig 의 memberService 객체를 가져오겠다는 의미
+         * */
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/main/java/hello/springcore/OrderApp.java
+++ src/main/java/hello/springcore/OrderApp.java
@@ -7,15 +7,31 @@ import hello.springcore.member.MemberServiceImpl;
 import hello.springcore.order.Order;
 import hello.springcore.order.OrderService;
 import hello.springcore.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
-//        MemberService memberService = new MemberServiceImpl();
-//        OrderService orderService = new OrderServiceImpl();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
+        /*
+        * AppConfig 의 memberService, orderService 객체 호출
+        *
+        * @Bean
+        * public MemberService memberService(){
+        *     return new MemberServiceImpl(memberRepository());
+        * }
+        *
+        * @Bean
+        * public OrderService orderService(){
+        *     return new OrderServiceImpl(memberRepository(), discountPolicy());
+        * }
+        * */
 
         long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
