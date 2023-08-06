110b99efa016e4bbee0714842eb9cc6ea61b2133
ro-el <alcxzp@gmail.com>
2023-08-06 22:13:35 +0900

    test: add test code

--- src/test/java/hello/springcore/order/OrderServiceImplTest.java
+++ src/test/java/hello/springcore/order/OrderServiceImplTest.java
@@ -1,13 +1,22 @@
 package hello.springcore.order;
 
+import hello.springcore.discount.FixDiscountPolicy;
+import hello.springcore.member.Grade;
+import hello.springcore.member.Member;
+import hello.springcore.member.MemoryMemberRepository;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderServiceImplTest {
     @Test
     void createOrder() {
-        OrderServiceImpl orderService = new OrderServiceImpl();
-        orderService.createOrder(1L, "itemA", 10000);
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 }
