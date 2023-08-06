99401d641548b4b6a5bde6fa31164f5f331d238a
ro-el <alcxzp@gmail.com>
2023-08-06 17:28:10 +0900

    6. Component Scan - Bean Name Conflict (auto vs manual)

--- src/main/java/hello/springcore/AutoAppConfig.java
+++ src/main/java/hello/springcore/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.springcore;
 
+import hello.springcore.member.MemberRepository;
+import hello.springcore.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,5 +12,8 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 ) // 스프링 빈으로 제대로 등록되는지 확인 + 앞서 작성한 코드를 최대한 유지하기 위하여 @Configuration 제외하고 스프링 빈 등록
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
