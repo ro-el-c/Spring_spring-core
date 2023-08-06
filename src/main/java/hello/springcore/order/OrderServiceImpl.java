eec676cb23e17caa8144319ebf03ca75851daa6d
ro-el <alcxzp@gmail.com>
2023-08-06 18:26:14 +0900

    7. Dependency Injection - Constructor Injection

--- src/main/java/hello/springcore/order/OrderServiceImpl.java
+++ src/main/java/hello/springcore/order/OrderServiceImpl.java
@@ -11,11 +11,12 @@ import org.springframework.stereotype.Component;
 
 @Component("orderServiceImplBean")
 public class OrderServiceImpl implements OrderService{
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+    // final - 바로 혹은 생성자를 통해 할당되어야 함
 
     @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
