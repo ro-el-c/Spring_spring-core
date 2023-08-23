2ca11450eb972f7cbb867d72842c8ac05dd66b02
ro-el-c <chung3286@naver.com>
2023-08-23 17:17:31 +0900

    8. Bean LifeCycle CallBack - Interface(InitializingBean, DisposableBean)

--- src/test/java/hello/springcore/lifecycle/NetworkClient.java
+++ src/test/java/hello/springcore/lifecycle/NetworkClient.java
@@ -1,13 +1,13 @@
 package hello.springcore.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-        disconnect();
     }
 
     public void setUrl(String url) {
@@ -26,5 +26,20 @@ public class NetworkClient {
     // 서비스 종료시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나면 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        // 소멸 직전 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
