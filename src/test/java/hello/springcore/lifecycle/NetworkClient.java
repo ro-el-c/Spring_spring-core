3c6e46866da3535620eac3311df8e9a34270d8fb
ro-el-c <chung3286@naver.com>
2023-08-23 17:38:58 +0900

    8. Bean LifeCycle CallBack - Setting init, close Method

--- src/test/java/hello/springcore/lifecycle/NetworkClient.java
+++ src/test/java/hello/springcore/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.springcore.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
 
     public NetworkClient() {
@@ -28,18 +25,16 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
         // 의존관계 주입이 끝나면 호출
-        System.out.println("NetworkClient.afterPropertiesSet");
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
         // 소멸 직전 호출
-        System.out.println("NetworkClient.destroy");
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
