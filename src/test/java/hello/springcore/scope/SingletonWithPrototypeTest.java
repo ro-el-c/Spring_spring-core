5d69be6873f9a72a0120c76b03772105d5c5d5d7
ro-el <alcxzp@gmail.com>
2023-08-26 21:05:23 +0900

    9. Bean Scope - Singleton With Prototype - Provider

--- src/test/java/hello/springcore/scope/SingletonWithPrototypeTest.java
+++ src/test/java/hello/springcore/scope/SingletonWithPrototypeTest.java
@@ -1,12 +1,15 @@
 package hello.springcore.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,20 +43,28 @@ public class SingletonWithPrototypeTest {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int cnt2 = clientBean2.logic();
-        assertThat(cnt2).isEqualTo(2);
+//        assertThat(cnt2).isEqualTo(2);
+        assertThat(cnt2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입 => 계속 같은 빈을 사용하게 됨
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입 => 계속 같은 빈을 사용하게 됨
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            // 스프링 컨테이너가 프로토타입 빈을 만들어 주입
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            // 스프링 컨테이너가 프로토타입 빈을 만들어 주입
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject(); // ObjectProvider 또는 ObjectFactory 일 때
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
